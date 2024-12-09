89be88f893b26fa29c072f07cfb3b402ebfe4dae
Lviat Yi <LviatYi@foxmail.com>
2024-12-09 15:47:10 +0800

    feat: add default config

---
+++ src/default_config/mod.rs
@@ -0,0 +1,8 @@
+pub const DEFAULT_REPO_TEMPLATE: &str = "";
+pub const DEFAULT_LOCATOR_PATTERN: &str = "";
+pub const DEFAULT_LOCATOR_TEMPLATE: &str = "";
+pub const MENDING_FILE_PATH: &str = "";
+
+pub const DEFAULT_PACKAGE_FILE_STEM: &str = "";
+pub const DEFAULT_EXE_FILE_NAME: &str = "";
+pub const DEFAULT_CHECK_EXE_FILE_NAME: &str = "";
