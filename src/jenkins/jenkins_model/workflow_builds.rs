78dbecb726ccef65c361c5e0a24a79ea52346bd6
Lviat Yi <LviatYi@foxmail.com>
2025-06-09 16:18:38 +0800

    feat: impl IntoIterator for WorkflowBuilds

--- src/jenkins/jenkins_model/workflow_builds.rs
+++ src/jenkins/jenkins_model/workflow_builds.rs
@@ -1,7 +1,26 @@
 use crate::jenkins::jenkins_model::workflow_build::WorkflowBuild;
 use serde::Deserialize;
+use std::slice::{Iter, IterMut};
 
 #[derive(Deserialize, Debug)]
 pub struct WorkflowBuilds {
     pub builds: Vec<WorkflowBuild>,
+}
+
+impl<'a> IntoIterator for &'a WorkflowBuilds {
+    type Item = &'a WorkflowBuild;
+    type IntoIter = Iter<'a, WorkflowBuild>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.builds.iter()
+    }
+}
+
+impl<'a> IntoIterator for &'a mut WorkflowBuilds {
+    type Item = &'a mut WorkflowBuild;
+    type IntoIter = IterMut<'a, WorkflowBuild>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.builds.iter_mut()
+    }
 }
